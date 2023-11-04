0681ebb549d6ea56d2dc0887bd8b348efda275d9
xv <iberkstresser@protonmail.com>
2023-11-03 22:23:59 -0500

    Adding player ghost mode and separating movement script

---
+++ Assets/Scripts/PlayerCoreController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerController : MonoBehaviour
+{
+    readonly Color COLOR_MAIN = new(1f, .75f, .25f);
+    readonly Color COLOR_GHOST = new(.25f, .75f, 1f);
+
+    [Header("General")]
+    public Vector2 spawnPoint = new(-8f, 0f);
+    public bool ghostMode = false;
+
+    [Header("References")]
+    public SpriteRenderer spriteRendererComponent;
+    public Light2D light2dComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Figure out which color to use
+        var color = ghostMode ? COLOR_GHOST : COLOR_MAIN;
+
+        // Apply that color to the square and light
+        spriteRendererComponent.color = color;
+        light2dComponent.color = color;
+
+        // If the player is a ghost, disable gravity and movement
+        if (ghostMode)
+        {
+            GetComponent<Rigidbody2D>().simulated = false;
+        }
+        // If the player is not a ghost, add the movement controller script
+        else
+        {
+            var _ = gameObject.AddComponent(typeof(PlayerMovement)) as PlayerMovement;
+        }
+    }
+
+    // Reset the player back to the spawn point
+    public void Kill()
+    {
+        transform.position = spawnPoint;
+    }
+}
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,15 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float moveForce = 3f;
+    [Header("General")]
+    public float moveSpeed = 10f;
+    public float moveForce = 10f;
     public float jumpForce = 10f;
-
-    public Vector2 spawnPoint = new(-8f, 0f);
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -17,23 +15,23 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Save a reference to the rigidbody component
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Check for ground contact to allow jumping
+        // Check if on the ground
         isGrounded = Physics2D.Raycast(transform.position - new Vector3(0f, 0.51f), Vector2.down, 0.1f);
 
-        // Player movement
+        // Apply movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         rb.AddForce(new Vector2(horizontalInput * moveSpeed - rb.velocity.x, 0f) * moveForce);
 
-        // Jumping
-        Debug.Log(isGrounded);
+        // Apply jump
         if (isGrounded && verticalInput > 0)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
