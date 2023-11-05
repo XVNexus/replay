e38cdf6d5b8ebdab915190312b461311426576a6
xv <iberkstresser@protonmail.com>
2023-11-05 00:19:24 -0500

    Kinda fixed jump cheesing

--- Assets/Scripts/Controllers/SensorController.cs
+++ Assets/Scripts/Controllers/SensorController.cs
@@ -10,7 +10,19 @@ public class SensorController : MonoBehaviour
         }
     }
 
+    public void Start()
+    {
+        // Subscribe to events
+        EventSystem.current.OnPlayerDie += OnPlayerDie;
+    }
+
     // Keep track of how many objects are hitting this trigger to see if the player is on the ground
-    void OnTriggerEnter2D() => overlaps++;
-    void OnTriggerExit2D() => overlaps--;
+    public void OnTriggerEnter2D() => overlaps++;
+    public void OnTriggerExit2D() => overlaps--;
+
+    // When the player dies, reset overlapping object counter to avoid issues with sensor always being triggered
+    public void OnPlayerDie(Vector2 spawnpoint, Vector2[] positionHistory)
+    {
+        overlaps = 0;
+    }
 }
